88f85a0a0ebf56782dfb54e80191583bd93b6372
DomD3331 <domskate320@gmail.com>
2026-06-24 10:09:36 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -23,6 +23,15 @@ const commands = [
       .setDescription("Your idea")
       .setRequired(true)
   ),
+  new SlashCommandBuilder()
+  .setName("bug")
+  .setDescription("Report a bug")
+  .addStringOption(option =>
+    option
+      .setName("issue")
+      .setDescription("Describe the bug")
+      .setRequired(true)
+  ),
 new SlashCommandBuilder()
   .setName("help")
   .setDescription("Show SparkAI Bot commands"),
